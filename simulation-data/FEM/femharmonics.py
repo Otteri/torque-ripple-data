6469561fcf0da96efde47288f173a8011cbd635f
Joni Airaksinen <airakjo@gmail.com>
2020-04-25 02:42:45 +0300

    Replace frequency with harmonic order in FEM plots

--- simulation-data/FEM/femharmonics.py
+++ simulation-data/FEM/femharmonics.py
@@ -7,11 +7,11 @@ from glob import glob
 
 DPI = 60
 
-def FluxHarmonics(X, Y, fundamental_hz):
+def fluxHarmonics(X, Y, fundamental_hz):
 
     fig, ax = plt.subplots(figsize=(16,10), dpi=DPI)
     ax.vlines(x=X, ymin=0, ymax=Y, color='mediumblue', alpha=0.7, linewidth=6)
-    Y[0:12] = -500 # Hide from plot, so axis break shows nicely
+    #Y[0:12] = -500 # Hide from plot, so axis break shows nicely
     ax.scatter(x=X, y=Y, s=80, color='mediumblue', alpha=0.8)
     plt.tick_params(labelsize=16)
     ax.set_ylabel('Amplitude [Nm]', fontsize=20)
@@ -19,8 +19,6 @@ def FluxHarmonics(X, Y, fundamental_hz):
 
     # Flux harmonics
     ax.set(xlim=(810, 5000), ylim=(-0.5, 25))
-    ax.set_yticks(np.arange(0,25, step=2.5))
-
     ax.text(0, Y[0]+0.1, s='0th', horizontalalignment='center', verticalalignment='bottom', fontsize=16)
     ax.text(930, Y[12]+0.6, s='6th', horizontalalignment='center', verticalalignment='bottom', fontsize=16)
     ax.text(1860, Y[24]+0.6, s='12th', horizontalalignment='center', verticalalignment='bottom', fontsize=16)
@@ -28,7 +26,7 @@ def FluxHarmonics(X, Y, fundamental_hz):
     ax.text(3720, Y[48]+0.6, s='24th', horizontalalignment='center', verticalalignment='bottom', fontsize=16)
     ax.text(4650, Y[60]+0.6, s='30th', horizontalalignment='center', verticalalignment='bottom', fontsize=16)
 
-def CoggingHarmonics(X, Y, fundamental_hz):
+def coggingHarmonics(X, Y, fundamental_hz):
     fig, ax = plt.subplots(figsize=(16,10), dpi=DPI)
     ax.vlines(x=X, ymin=0, ymax=Y, color='mediumblue', alpha=0.7, linewidth=6)
     ax.scatter(x=X, y=Y, s=80, color='mediumblue', alpha=0.8)
@@ -38,31 +36,30 @@ def CoggingHarmonics(X, Y, fundamental_hz):
     # Cogging harmonics
     plt.tick_params(labelsize=16)
     ax.set(xlim=(-150, 8000), ylim=(-0.14, 6))
-    ax.set_yticks(np.arange(0,6, step=0.5))
 
     ax.text(0, Y[0]+0.1, s='0th', horizontalalignment='center', verticalalignment='bottom', fontsize=16)
     ax.text(3720, Y[24]+0.1, s='24th', horizontalalignment='center', verticalalignment='bottom', fontsize=16)
     ax.text(7440, Y[48]+0.1, s='48th', horizontalalignment='center', verticalalignment='bottom', fontsize=16)
 
 # Plot harmonic order on x-axle and amplitude on y-axle.
-def Harmonics(X, Y, fundamental_hz):
+def harmonics(X, Y):
     X2, Y2 = [], []
-    for i in range(0, 50, 1):
+    for i in range(0, 49, 1):
         Y2.append(Y[i])
         X2.append(i)
 
     fig, ax = plt.subplots(figsize=(16,10), dpi=DPI)
     ax.vlines(x=X2, ymin=0, ymax=Y2, color='mediumblue', alpha=0.7, linewidth=6)
     ax.scatter(x=X2, y=Y2, s=80, color='mediumblue', alpha=0.8)
-    ax.set_ylabel('Amplitude [Nm]', fontsize=20)
-    ax.set_xlabel('Harmonic order', fontsize=20)
+    ax.set_ylabel('Torque amplitude [Nm]', fontsize=20)
+    ax.set_xlabel('Harmonic order n', fontsize=20)
 
-    ax.set_yticks(np.arange(0,6, step=0.5))
+    #ax.set_yticks(np.arange(0,6, step=0.5))
     ax.set_xticks(np.arange(0, 50, step=2))
     ax.set_xticks(X2[1::2], minor=True)
-    ax.tick_params(which='major', length=5)
+    ax.tick_params(which='major', length=6)
     ax.tick_params(which='minor', length=4)
-
+    ax.tick_params(labelsize=16)
 
 def main():
     # Fundamental frequency of the motor
@@ -80,13 +77,12 @@ def main():
     torques2 = df2['torque'].to_numpy()
 
     # Plot the harmonics
-    CoggingHarmonics(times1, torques1, fundamental_hz=fn)
+    coggingHarmonics(times1, torques1, fundamental_hz=fn)
+    harmonics(times1, torques1)
     plt.savefig("cogging-harmonics.svg")
 
-    Harmonics(times1, torques1, fundamental_hz=fn)
-    plt.savefig("harmonics.svg")
-
-    FluxHarmonics(times2, torques2, fundamental_hz=fn)
+    fluxHarmonics(times2, torques2, fundamental_hz=fn)
+    harmonics(times2, torques2)
     plt.savefig("flux-harmonics.svg")
 
 
